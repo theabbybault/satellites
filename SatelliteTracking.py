cc70b6dac68a2c336de3727d81545cea7a917538
Abby Bault <47161792+theabbybault@users.noreply.github.com>
2019-07-26 16:13:00 -0700

    Update SatelliteTracking.py

--- SatelliteTracking.py
+++ SatelliteTracking.py
@@ -1,47 +1,136 @@
-def load_satellites(reload=True):
-    satellites = {}
-    added = set()
-    for group in (
-        # Weather & earth resources
-        'weather', 'noaa', 'goes', 'resource', 'sarsat', 'dmc', 'tdrss', 'argos', 'planet', 'spire',
-        # Communications
-        'geo', 'gpz', 'gpz-plus', 'intelsat', 'ses', 'iridium', 'iridium-NEXT', 'starlink', 'orbcomm', 'globalstar', 
-        'amateur', 'x-comm', 'other-comm', 'satnogs', 'gorizont', 'raduga', 'molniya',
-        # Navigation
-        'gps-ops', 'glo-ops', 'galileo', 'beidou', 'sbas', 'nnss', 'musson',
-        # Scientific
-        'science', 'geodetic', 'engineering', 'education',
-        # Miscellaneous
-        'military', 'radar', 'cubesat', 'other',
-        # Debris
-        '2019-006', '1999-025', 'iridium-33-debris', 'cosmos-2251-debris', '2012-044',
-        # Special interest (should already be included above)
-        'stations', 'visual', 'active', 'analyst'):
-        loaded = skyfield.api.load.tle(f'http://celestrak.com/NORAD/elements/{group}.txt', reload=reload)
-        unique = set(loaded.values())
-        toadd = unique - added
-        nadded = 0
-        for satellite in toadd:
-            if satellite.target_name in satellites:
-                # Only replace with a more recent epoch.
-                if satellite.epoch.tai <= satellites[satellite.target_name].epoch.tai:
-                    continue
-            satellites[satellite.target_name] = satellite
-            added.add(satellite)
-            nadded += 1
-        print(f'Added {nadded} satellites from {group}.')
-    print(f'Found {len(satellites)} satellites')
-    return satellites
+'''This file is for all of the satellite functions to be in one place to not have to keep staying
+in the same notebook'''
+'''Quick snapshot of all functions in this file
+load_satellites
+get_satellites
+from_localtime
+separation_matrix # this is from another file but is needed for other functions
+get_pos
+get_visible
+'''
+#importing all necessary functions
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import pylab, mlab
+import scipy.interpolate
+import skyfield.api
+import skyfield.toposlib
+from skyfield.api import Time
+import datetime
+
+#Globals
+ts=skyfield.api.load.timescale()
 
 
+def load_satellites(reload = True, groups = None, debris = True):
+	"""reload defaulted to true, when running in jupyter set to False.
+	When groups is set to None, all of the groups from celestrak will be included in the dictionary. To specify groups, find the filename from celestrak and set 
+	groups = ('group1','group2', 'groupx'). 
+	Debris will be included if groups = None OR if debris = True. Debris will be default to True, and will only be excluded if debris=False. 
+	If specifying groups, do not set debris =True or False."""
+	satellites = {}
+	added = set()
+	if groups is not None:
+		for group in groups:
+			loaded=skyfield.api.load.tle(f'http://celestrak.com/NORAD/elements/{group}.txt', reload=reload)
+			unique=set(loaded.values())
+			toadd=unique-added
+			nadded=0
+			for satellite in toadd:
+				if satellite.target_name in satellites:
+					if satellite.epoch.tai <= satellites[satellite.target_name].epoch.tai:
+						continue
+				satellites[satellite.target_name] = satellite
+				added.add(satellite)
+				nadded+=1
+			print(f'Added {nadded} satellites from {group}.')
+		print(f'Found {len(satellites)} satellites')
+		return satellites
+	if groups is None:
+		if debris is True:
+			for group in (
+			# Weather & earth resources
+			'weather', 'noaa', 'goes', 'resource', 'sarsat', 'dmc', 'tdrss', 'argos', 'planet', 'spire',
+			# Communications
+			'geo', 'gpz', 'gpz-plus', 'intelsat', 'ses', 'iridium', 'iridium-NEXT', 'starlink', 'orbcomm', 'globalstar',
+			'amateur', 'x-comm', 'other-comm', 'satnogs', 'gorizont', 'raduga', 'molniya',
+			# Navigation
+			'gps-ops', 'glo-ops', 'galileo', 'beidou', 'sbas', 'nnss', 'musson',
+			# Scientific
+			'science', 'geodetic', 'engineering', 'education',
+			# Miscellaneous
+			'military', 'radar', 'cubesat', 'other',
+			# Debris
+			'2019-006', '1999-025', 'iridium-33-debris', 'cosmos-2251-debris', '2012-044',
+			# Special interest (should already be included above)
+			'tle-new','stations', 'visual', 'active', 'analyst'):
+				loaded=skyfield.api.load.tle(f'http://celestrak.com/NORAD/elements/{group}.txt', reload=reload)
+				unique=set(loaded.values())
+				toadd=unique-added
+				nadded=0
+				for satellite in toadd:
+					if satellite.target_name in satellites:
+						if satellite.epoch.tai <= satellites[satellite.target_name].epoch.tai:
+							continue
+					satellites[satellite.target_name] = satellite
+					added.add(satellite)
+					nadded+=1
+				print(f'Added {nadded} satellites from {group}.')
+			print(f'Found {len(satellites)} satellites')
+			return satellites
+		if debris is False:
+			for group in (
+			# Weather & earth resources
+			'weather', 'noaa', 'goes', 'resource', 'sarsat', 'dmc', 'tdrss', 'argos', 'planet', 'spire',
+			# Communications
+			'geo', 'gpz', 'gpz-plus', 'intelsat', 'ses', 'iridium', 'iridium-NEXT', 'starlink', 'orbcomm', 'globalstar',
+			'amateur', 'x-comm', 'other-comm', 'satnogs', 'gorizont', 'raduga', 'molniya',
+			# Navigation
+			'gps-ops', 'glo-ops', 'galileo', 'beidou', 'sbas', 'nnss', 'musson',
+			# Scientific
+			'science', 'geodetic', 'engineering', 'education',
+			# Miscellaneous
+			'military', 'radar', 'cubesat', 'other',
+			# Special interest (should already be included above)
+			'tle-new','stations', 'visual', 'active', 'analyst'):
+				loaded=skyfield.api.load.tle(f'http://celestrak.com/NORAD/elements/{group}.txt', reload=reload)
+				unique=set(loaded.values())
+				toadd=unique-added
+				nadded=0
+				for satellite in toadd:
+					if satellite.target_name in satellites:
+						if satellite.epoch.tai <= satellites[satellite.target_name].epoch.tai:
+							continue
+					satellites[satellite.target_name] = satellite
+					added.add(satellite)
+					nadded+=1
+				print(f'Added {nadded} satellites from {group}.')
+			print(f'Found {len(satellites)} satellites')
+			return satellites
 
-def get_satellites(string):
-    found = [name for name in satellites if string in name]
-    return {name: satellites[name] for name in found}
+
+def get_satellites(satellites, string):
+	''' Returns the satellite data(name, type?, target_name, number, and epoch, for any
+	satellite with the string in its name.
+	Parameters
+	----------
+	string: string, example: 'ISS'
+	Returns
+	-------
+	List of satellites data (name, type?, target_name, number, and epoch)
+	with 'string' in its name.
+	Notes 
+	-----
+	Must have defined satellites from load_satellites first
+	any satellite with the string in its name will be returned. For example,
+	both ISS DEB and ISS (ZARYA) will be returned with input as 'ISS'.'''
+	found = [name for name in satellites if string in name]
+	return {name: satellites[name] for name in found}
 
 
 def from_localtime(*args, tzoffset = -7):
-    return ts.tai_jd(ts.utc(*args).tai - tzoffset / 24)
+	ts=skyfield.api.load.timescale()
+	return ts.tai_jd(ts.utc(*args).tai - tzoffset / 24)
 
 
 def get_pos(where, when, string):
@@ -54,7 +143,6 @@ def get_pos(where, when, string):
         # Calculate astrometric (ra, dec)
         ra, dec, _ = topocentric.radec()
         print(satellite.name, 'ALT', alt.degrees, 'AZ', az.degrees, 'RA', ra._degrees, 'DEC', dec._degrees)
-
 
 
 def separation_matrix(ra1, dec1, ra2, dec2, max_separation=None):
@@ -107,60 +195,82 @@ def separation_matrix(ra1, dec1, ra2, dec2, max_separation=None):
         return np.rad2deg(np.arccos(np.clip(1 - 2 * havPHI, -1, +1)))
 
 
-
-def get_visible(where, tbegin, exptime, ra0, dec0, fov=3.5, satellites=satellites, nsteps=10, oversampling=5):
-    """
-    """
-    cos_dec0 = np.cos(np.deg2rad(dec0))
-    # Calculate the end time of this exposure.
-    tend = ts.utc(tbegin.utc_datetime() + datetime.timedelta(seconds=exptime))
-    # Calculate the exposure midpoint.
-    tmid = ts.tai_jd(0.5 * (tbegin.tai + tend.tai))
-    # Calculate equal coarse time steps covering [tbegin, tend].
-    tcoarse = np.linspace(tbegin.tai, tend.tai, nsteps)
-    rcoarse = np.empty((3, nsteps))
-    tsteps = ts.tai_jd(tcoarse)
-    # Build a fine grid of time steps during the exposure for interpolation.
-    tfine = np.linspace(tbegin.tai, tend.tai, oversampling * nsteps)
-    # Loop over satellites.
-    names, ravec, decvec = [], [], []
-    for i, satellite in enumerate(satellites.values()):
-        # Calculate satellite position relative to the observer at each time step.
-        topocentric = (satellite - where).at(tsteps)
-        # Calculate the corresponding astrometric (ra, dec)
-        ra, dec, _ = topocentric.radec()
-        # Check for propagation errors.
-        if np.any(np.isnan(ra.radians) | np.isnan(dec.radians)):
-            #print(f'Unable to propagate {satellite.name}')
-            continue
-        # Tabulate the satellite position on the finer grid using cubic interpolation.
-        rcoarse[0] = np.sin(dec.radians)
-        rcoarse[1] = np.cos(ra.radians)
-        rcoarse[2] = np.sin(ra.radians)
-        interpolator = scipy.interpolate.interp1d(
-            tcoarse, rcoarse, axis=1, kind='cubic', copy=False,
-            bounds_error=False, assume_sorted=True)
-        sin_dec, cos_ra, sin_ra = interpolator(tfine)
-        dec_deg = np.rad2deg(np.arcsin(sin_dec))
-        ra_deg = np.fmod(360 + np.rad2deg(np.arctan2(sin_ra, cos_ra)), 360)
-        # Does this satellite ever enter the fov?
-        #sep = separation_matrix([ra0], [dec0], ra_deg, dec_deg, max_separation=0.6 * fov).reshape(-1)
-        sep = separation_matrix([ra0], [dec0], ra_deg, dec_deg).reshape(-1)
-        if sep.min() < 2 * fov:
-            plt.plot(ra_deg, dec_deg, '.-', label=satellite.name)
-            names.append(satellite.name)
-            ravec.append(ra_deg)
-            decvec.append(dec_deg)
-    plt.xlim(ra0 - 2 * fov / cos_dec0, ra0 + 2 * fov / cos_dec0)
-    plt.ylim(dec0 - 2 * fov, dec0 + 2 * fov)
-    ax = plt.gca()
-    ax.invert_xaxis()
-    ax.set_aspect(1 / cos_dec0)
-    # Draw circle showing fov.
-    theta = np.linspace(0, 2 * np.pi, 50)
-    x = ra0 + 0.5 * fov * np.cos(theta) / cos_dec0
-    y = dec0 + 0.5 * fov * np.sin(theta)
-    ax.plot(x, y, ls=':', c='gray', label='FOV')
-    plt.legend()
-    plt.grid()
-    return names, ravec, decvec
+def get_visible(where, tbegin, exptime, ra0, dec0, fov, satellites, title, string=None, nsteps=10, oversampling=5, location = None):
+	"""
+	"""
+	if string is not None:
+		satellites={}
+		for name in string:
+			satellites.update(get_satellites(name))
+			#append(get_satellites(name))
+		#if string == None:
+		#satellites=load_satellites(reload=False)  #if I try this as satellites=satellites I get an assignment error.
+		#satellites=satellites
+	#else:
+	#    satellites=get_satellites(string)
+	cos_dec0 = np.cos(np.deg2rad(dec0))
+	# Calculate the end time of this exposure.
+	tend = ts.utc(tbegin.utc_datetime() + datetime.timedelta(seconds=exptime))
+	# Calculate the exposure midpoint.
+	tmid = ts.tai_jd(0.5 * (tbegin.tai + tend.tai))
+	# Calculate equal coarse time steps covering [tbegin, tend].
+	tcoarse = np.linspace(tbegin.tai, tend.tai, nsteps)
+	rcoarse = np.empty((3, nsteps))
+	tsteps = ts.tai_jd(tcoarse)
+	# Build a fine grid of time steps during the exposure for interpolation.
+	tfine = np.linspace(tbegin.tai, tend.tai, oversampling * nsteps)
+	# Loop over satellites.
+	names, ravec, decvec = [], [], []
+	plt.figure(figsize=(10,10))
+	for i, satellite in enumerate(satellites.values()):
+		# Calculate satellite position relative to the observer at each time step.
+		topocentric = (satellite - where).at(tsteps)
+		# Calculate the corresponding astrometric (ra, dec)
+		ra, dec, _ = topocentric.radec()
+		# Check for propagation errors.
+		if np.any(np.isnan(ra.radians) | np.isnan(dec.radians)):
+			#print(f'Unable to propagate {satellite.name}')
+			continue
+		# Tabulate the satellite position on the finer grid using cubic interpolation.
+		rcoarse[0] = np.sin(dec.radians)
+		rcoarse[1] = np.cos(ra.radians)
+		rcoarse[2] = np.sin(ra.radians)
+			#'''Why is cos(dec) not used?'''
+		interpolator = scipy.interpolate.interp1d(
+			tcoarse, rcoarse, axis=1, kind='cubic', copy=False,
+			bounds_error=False, assume_sorted=True)
+		sin_dec, cos_ra, sin_ra = interpolator(tfine)
+		dec_deg = np.rad2deg(np.arcsin(sin_dec))
+		ra_deg = np.fmod(360 + np.rad2deg(np.arctan2(sin_ra, cos_ra)), 360)
+		# Does this satellite ever enter the fov?
+		#sep = separation_matrix([ra0], [dec0], ra_deg, dec_deg, max_separation=0.6 * fov).reshape(-1)
+		sep = separation_matrix([ra0], [dec0], ra_deg, dec_deg).reshape(-1)
+		if sep.min() < 2 * fov:
+			plt.plot(ra_deg, dec_deg, '.-', label=satellite.name)
+			#names.append(satellite.name)
+			ravec.append(ra_deg)
+			decvec.append(dec_deg)
+		if string is not None:
+			names.append(satellite.name)
+	#plt.xlim(ra0 - 2 * fov / cos_dec0, ra0 + 2 * fov / cos_dec0)
+	#plt.ylim(dec0 - 2 * fov, dec0 + 2 * fov)
+	plt.xlabel('RA (deg)')
+	plt.ylabel('DEC (deg)')
+	plt.title(title)
+	ax = plt.gca()
+	ax.invert_xaxis()
+	ax.set_aspect(1 / cos_dec0)
+	#plt.xlim(ra0+fov, ra0-fov, 10)
+	#plt.ylim(dec0-fov, dec0+fov, 10)
+	# Draw circle showing fov.
+	#theta = np.linspace(0, 2 * np.pi, 50)
+	#x = ra0 + 0.5 * fov * np.cos(theta) / cos_dec0
+	#y = dec0 + 0.5 * fov * np.sin(theta)
+	#ax.plot(x, y, ls=':', c='gray', label='FOV')
+	if location is not None:
+		plt.legend(loc=location)
+	else:
+		plt.legend(loc='best')
+	plt.grid()
+	plt.savefig(title)
+	return names, ravec, decvec
